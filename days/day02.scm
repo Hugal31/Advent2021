ee5b7f3be10609177eb0cd59b891b003904a9b88
Hugo Laloge <hla@lescompanions.com>
2021-12-02 16:33:08 +0100

    Add unit testsing over day02's parsing

--- days/day02.scm
+++ days/day02.scm
@@ -49,12 +49,15 @@
   (stream->list
    (stream-map (lambda (line)
                  (match (string-split line #\space)
+                   ((not (_ (? (lambda (txt) (integer? (string->number txt 10))))))
+                    (error "Each line must be a string and a number separated by a line" line))
+
                    (("forward" amount)
-                    `(forward . ,(string->number amount)))
+                    `(forward . ,(string->number amount 10)))
                    (("up" amount)
-                    `(up . ,(string->number amount)))
+                    `(up . ,(string->number amount 10)))
                    (("down" amount)
-                    `(down . ,(string->number amount)))))
+                    `(down . ,(string->number amount 10)))))
                (parse-lines-file-stream input))))
 
 (define-public (unit-tests)
@@ -65,6 +68,27 @@
            (up . 3)
            (down . 8)
            (forward . 2))))
+
+    (suite "parse-instructions"
+           (tests
+            (test "should parse null list"
+                  e
+                  (assert-equal '() (call-with-input-string "" parse-instructions)))
+            (test "should parse all instructions"
+                  e
+                  (assert-equal '((forward . 1)
+                                  (up . 2)
+                                  (down . 3))
+                                (call-with-input-string "forward 1
+up 2
+down 3"
+                                  parse-instructions)))
+            (test "should throw error on invalid tet"
+                  e
+                  (assert-all
+                   (assert-true (error? (call-with-input-string "up b" parse-instructions)))
+                   (assert-true (error? (call-with-input-string "forward c" parse-instructions)))
+                   (assert-true (error? (call-with-input-string "down 1.2" parse-instructions)))))))
 
     (suite "solve1"
            (tests
